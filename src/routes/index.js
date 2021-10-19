28eef1b9ab4578e9c38d82b72236d0ccb1f24b03
nukezzzz <lawthenuka@gmail.com>
2021-10-20 01:50:21 +0530

    cmd function

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,29 @@ router.get('/', (req, res) => {
   res.send({ message: 'Hello world' });
 });
 
+router.get('/hi', (req, res) => {
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function runCommand(command) {
+  const { stdout, stderr, error } = await exec(command);
+  if(stderr){console.error('stderr:', stderr);}
+  if(error){console.error('error:', error);}
+  return stdout;
+}
+
+
+async function myFunction () {
+    // your code here building the command you wish to execute ...
+    const command = 'hostname';
+    const result = await runCommand(command);
+    res.send(result);
+    // your code here processing the result ...
+}
+
+// just calling myFunction() here so it runs when the file is loaded
+myFunction();
+  
+});
+
 module.exports = router;
